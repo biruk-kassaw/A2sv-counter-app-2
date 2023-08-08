f3caef4ecf557fa6c96ae7a4c92371896ef112cc
biruk-kassaw <61546383+biruk-kassaw@users.noreply.github.com>
2023-08-08 10:14:24 +0300

    2 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
-import * as React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './style.css';
 
-export default function App() {
+function App() {
+  // useState Hook to manage the counter value
+  const [counter, setCounter] = useState(0);
+
+  // useEffect Hook to update the document title
+  useEffect(() => {
+    document.title = `Counter: ${counter}`;
+  }, [counter]);
+
+  // useRef Hook to access the DOM button element
+  const buttonRef = useRef(null);
+
+  // Function to increment the counter
+  const increment = () => {
+    setCounter((prevCounter) => prevCounter + 1);
+  };
+
+  // Function to decrement the counter
+  const decrement = () => {
+    setCounter((prevCounter) => prevCounter - 1);
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App container">
+      <h1 className="m-2">Counter App with Hooks</h1>
+      <p className="m-2">Counter: {counter}</p>
+      <button
+        className="btn btn-primary m-2"
+        ref={buttonRef}
+        onClick={increment}
+      >
+        Increment
+      </button>
+      <button className="btn btn-secondary" onClick={decrement}>
+        Decrement
+      </button>
     </div>
   );
 }
+
+export default App;
